a43305d06e90ee093b9577a92a8a2251c1f4028f
Vania Bogdan <bogdan.vania@gmail.com>
2022-06-27 20:58:44 +0300

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+  form: document.getElementById('search-form'),
+  gallery: document.getElementById('gallery'),
+  // buttonLoadMore: document.getElementsByClassName('load-more')[0],
+  elementForDownloadImgs: document.getElementById('elementForDownloadImgs'),
+  themeChanger: document.getElementById('themeChanger'),
+};
